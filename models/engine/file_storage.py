08b5a2388d8bdc5ab80b1db8b5189aa1948fde35
shaadibo <colbertrashaad@gmail.com>
2019-02-16 21:01:34 +0000

    imported newly created classes for serialization

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,12 @@ FileStorage seralizes instances to JSON file and deseralizes fils to instances
 '''
 import json
 from models.base_model import BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
 class FileStorage():
     '''
     serializes instances to a JSON file and deserializes JSON file to instances
