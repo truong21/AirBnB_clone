6ad8eb329e4f6f9f2ce2353c80305f0741378cd5
shaadibo <colbertrashaad@gmail.com>
2019-02-16 21:02:35 +0000

    adding new classes that inherit form BaseModel

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+user class that inherits from basemodel
+'''
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    '''
+    name: string - empty string
+    '''
+    name = ""
