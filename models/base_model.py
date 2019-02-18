dff42bf9066456f854d4160cc22e9141a858fab4
truong21 <543@holbertonschool.com>
2019-02-18 02:22:03 +0000

    pep8 fixes

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,8 @@ import uuid
 
 class BaseModel():
     '''
-    BaseModel class that defines all common attributes/methods for other classes
+    BaseModel class that defines all common attributes/methods
+    for other classes
     '''
     def __init__(self, *args, **kwargs):
         """ initialization of an BaseModel instance with attributes """
@@ -20,15 +21,15 @@ class BaseModel():
             models.storage.new(self)
         else:
             self.__dict__ = kwargs
-            self.created_at = datetime.strptime(self.created_at, 
+            self.created_at = datetime.strptime(self.created_at,
                                                 "%Y-%m-%dT%H:%M:%S.%f")
             self.updated_at = datetime.strptime(self.updated_at,
                                                 "%Y-%m-%dT%H:%M:%S.%f")
 
     def __str__(self):
         """ returns a str representation of a object """
-        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__))
 
     def save(self):
         '''updates updated_at with current datetime'''
