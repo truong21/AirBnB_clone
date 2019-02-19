1349b18aaf9979644fc662c178c8aed1bb490291
shaadibo <colbertrashaad@gmail.com>
2019-02-19 18:58:37 +0000

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -10,3 +10,7 @@ class Amenity(BaseModel):
     name: string -empty string
     '''
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """init method"""
+        super().__init__(*args, **kwargs)
