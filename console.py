a8539de3c5c682e1db7444f9dc73d50f5e8037d6
shaadibo <colbertrashaad@gmail.com>
2019-02-19 18:23:07 +0000

    pep8 fix

--- console.py
+++ console.py
@@ -3,9 +3,12 @@ import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
+
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
-    isClass = ["BaseModel", "User", "City", "Amenity", "Review", "State","Place"]
+    isClass = ["BaseModel", "User", "City", "Amenity", "Review",
+               "State", "Place"]
 
     def do_quit(self, args):
         'exit the program'
@@ -32,9 +35,10 @@ class HBNBCommand(cmd.Cmd):
             print(instance.id)
 
     def do_show(self, line):
-        ' prints str representation of an instance based on the class name and id'
+        ' prints str representation of an instance based on the class\
+        name and id'
         intake = line.split()
-        print (intake)
+        print(intake)
         if len(intake) == 0:
             print('** class name missing **')
         elif intake not in self.isClass:
@@ -45,15 +49,17 @@ class HBNBCommand(cmd.Cmd):
         key = str(intake[0]) + '.' + str(intake[1])
         showme = storage.all()
         print(showme[key])
-         
+
     def do_destroy(self, args):
         'deletes an instance based on the class name and id'
 
     def do_all(self, args):
-        'prints all str representation of all instances based or not on the class name'
+        'prints all str representation of all instances based or\
+        not on the class name'
 
     def do_update(self, args):
-        'updates instance based on the class name and id by adding or updating attribute'
+        'updates instance based on the class name and id by adding or\
+        updating attribute'
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
