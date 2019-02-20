6f3241aa3a7a7fd9fae2cbdccc43c4d6ce3a4a3b
truong21 <543@holbertonschool.com>
2019-02-20 05:34:51 +0000

    test for amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-class that inherits from basemodel
+Amenity class that inherits from basemodel
 '''
 from models.base_model import BaseModel
 
@@ -10,7 +10,3 @@ class Amenity(BaseModel):
     name: string -empty string
     '''
     name = ""
-
-    def __init__(self, *args, **kwargs):
-        """init method"""
-        super().__init__(*args, **kwargs)
